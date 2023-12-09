cb5cfa2c7e509de64ba2812ab7b3125d3df6cd9a
RTD <rtd@rtd.com>
2023-12-09 15:37:48 +0000

    added some small changes

--- Client/Client_C/Generic/hash_file.c
+++ Client/Client_C/Generic/hash_file.c
@@ -6,21 +6,38 @@
 #include "send_receive.h"
 
 void hash_file(SSL* ssl){
-        char * file = receive_data(ssl); 
-        send_data(ssl, "1");
-        unsigned char data[1024];
-        int bytes;
-        FILE *fp = fopen(file, "rb");
-        if (fp == NULL) {
-            fprintf(stderr, "Error opening file\n");
-            return;
-        }
-        while ((bytes = fread(data, 1, 1024, fp)) != 0) {
-            sha256(data, data);
-        }
-        fclose(fp);
-        printf("Hash: %s\n", data);
-        send_data(ssl, file);
-        send_data(ssl, data);
-        puts("Hash sent");
+    char * file = receive_data(ssl); 
+    send_data(ssl, "1");
+    unsigned char data[SHA256_DIGEST_LENGTH];
+    FILE *fp = fopen(file, "rb");
+    if (fp == NULL) {
+        fprintf(stderr, "Error opening file\n");
+        return;
     }
+    fseek(fp, 0, SEEK_END);
+    long file_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    unsigned char *file_data = malloc(file_size);
+    if (file_data == NULL) {
+        fprintf(stderr, "Error allocating memory\n");
+        fclose(fp);
+        return;
+    }
+    if (fread(file_data, 1, file_size, fp) != file_size) {
+        fprintf(stderr, "Error reading file\n");
+        fclose(fp);
+        free(file_data);
+        return;
+    }
+    fclose(fp);
+    sha256(file_data, data);
+    free(file_data);
+    printf("Hash: ");
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        printf("%02x", data[i]);
+    }
+    printf("\n");
+    send_data(ssl, file);
+    send_data(ssl, data);
+    puts("Hash sent");
+}
